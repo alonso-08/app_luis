53e4162101a3501344c8aa3ec4ed551fc0650a49
luis.lopez.977 <luis.lopez.977@LLOPEZ.beliveo.corp>
2021-12-06 16:36:40 -0600

    pool

--- database.js
+++ database.js
@@ -1,18 +1,9 @@
 const mysql=require("mysql")
-const mysqlconection=mysql.createConnection({
+const mysqlconection=mysql.createPool({
     host: 'us-cdbr-east-04.cleardb.com',
     user: 'b0785817b57ce9',
     password: 'd9dddeb8',
     database: 'heroku_7662354e71f5b0b'
-})
-mysqlconection.connect(function (error){
-    if(error){
-        console.log(error)
-        return
-    }else{
-        console.log("Conexion exitosa")
-    }
-    
 })
 
 module.exports=mysqlconection
